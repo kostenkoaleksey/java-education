9ac9888037b5f0ee047e8aef063e1cd8e9695598
Oleksii Kostenko <aleksey.kostenko@p2h.com>
2023-07-20 19:33:25 +0300

    Refactored tests

--- src/test/java/collections/linkedlist/MyLinkedListTest.java
+++ src/test/java/collections/linkedlist/MyLinkedListTest.java
@@ -4,18 +4,20 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import javax.swing.*;
+import java.lang.reflect.Field;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyLinkedListTest {
-    static Integer[] items = {10, 20, 30, 10};
+    static Integer[] initialItemsList = {10, 20, 30, 10, 15, 1, 2, 2, 6};
 
     static MyLinkedList<Integer> list;
 
     @BeforeEach
     void init() {
-        list = new MyLinkedList<Integer>(Arrays.asList(items));
+        list = new MyLinkedList<Integer>(Arrays.asList(initialItemsList));
     }
 
     @AfterEach
@@ -24,107 +26,100 @@ class MyLinkedListTest {
     }
 
     @Test
-    void clearTheList() {
+    void shouldHaveZeroSize_WhenListCleared() {
         list.clear();
         assertEquals(0, list.size());
     }
 
+
     @Test
-    @DisplayName("Check the list size is equal 0.")
-    void sizeIsNull() {
-        list.clear();
-        assertEquals(0, list.size());
+    void shouldReturnNonZeroValue_WhenListIsNotEmpty() {
+        assertEquals(initialItemsList.length, list.size());
     }
 
     @Test
-    @DisplayName("Check the list size is equal 3.")
-    void sizeNotNull() {
-        assertEquals(items.length, list.size());
-    }
-
-    @Test
-    void isEmpty() {
+    void shouldReturnTrue_WhenListIsEmpty() {
         list.clear();
         assertTrue(list.isEmpty());
     }
 
     @Test
-    void isNotEmpty() {
+    void shouldReturnFalse_WhenListIsNotEmpty() {
         assertFalse(list.isEmpty());
     }
 
     @ParameterizedTest
     @ValueSource(ints = {10, 30, 20})
-    void containsElement(int element) {
+    void shouldReturnTrue_WhenListContainsElement(int element) {
         assertTrue(list.contains(element));
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1, 300, 40})
-    void doesNotContainElement(int fakeElement) {
+    @ValueSource(ints = {12, 300, 40})
+    void shouldReturnFalse_WhenListDoesNotContainElement(int fakeElement) {
         assertFalse(list.contains(fakeElement));
     }
 
     @Test
-    void containsAllElements() {
-        List<Integer> collection = Arrays.asList(items);
+    void shouldReturnTrue_WhenListContainsAllElements() {
+        List<Integer> collection = Arrays.asList(initialItemsList);
         assertTrue(list.containsAll(collection));
     }
 
     @Test
-    void doesNotContainAllElements() {
+    void shouldReturnFalse_WhenListDoesNotContainAtLeastOneElementFromCollection() {
         List<Integer> collection = List.of(20, 40, 30);
         assertFalse(list.containsAll(collection));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {10})
-    void indexOfExistingElementStartingHead(Integer element) {
+    void shouldFindIndexOfElement_WhenElementExists(Integer element) {
         assertEquals(0, list.indexOf(element));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {100})
-    void indexOfNonExistingElementStartingHead(Integer element) {
+    void shouldNotFindIndexOfElement_WhenElementDoesNotExist(Integer element) {
         assertEquals(-1, list.indexOf(element));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {10})
-    void indexOfExistingElementStartingTail(Integer element) {
+    void shouldFindIndexOfElementStartingEnd_WhenElementExists(Integer element) {
         assertEquals(3, list.lastIndexOf(element));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {100})
-    void indexOfNonExistingElementStartingTail(Integer element) {
+    void shouldNotFindIndexOfElementStartingEnd_WhenElementDoesNotExist(Integer element) {
         assertEquals(-1, list.lastIndexOf(element));
     }
 
     @Test
-    void iteratorInstance() {
+    void shouldReturnIteratorInstance() {
         Iterator<Integer> iterator = list.iterator();
         assertTrue(iterator instanceof Iterator<Integer>);
     }
 
     @Test
-    void iteratorSameSequence() {
+    void shouldReturnIteratorWithSameElementsSequence() {
         Iterator<Integer> iterator = list.iterator();
         int i = 0;
         while (iterator.hasNext()) {
-            assertEquals(items[i], iterator.next());
+            assertEquals(initialItemsList[i], iterator.next());
             i++;
         }
     }
 
     @Test
-    void listIteratorInstance() {
+    void shouldReturnListIteratorInstance() {
         ListIterator<Integer> listIterator = list.listIterator();
         assertInstanceOf(ListIterator.class, listIterator);
     }
 
     @Test
-    void listIteratorSameSequence() {
+    void shouldReturnListIteratorWithSameElementsSequence() {
         ListIterator<Integer> listIterator = list.listIterator();
         int i = 0;
         while (listIterator.hasNext()) {
@@ -135,34 +130,33 @@ class MyLinkedListTest {
 
     @ParameterizedTest
     @ValueSource(ints = {1})
-    void listIteratorStartingIndex(int index) {
+    void shouldReturnListIteratorPointedToSpecificElement_IndexedToSecondElement(int index) {
         ListIterator<Integer> listIterator = list.listIterator(index);
-        while (listIterator.hasNext()) {
-            assertEquals(listIterator.next(), list.get(index));
-            index++;
-        }
+
+        assertEquals(listIterator.previousIndex(), index -1);
+        assertEquals(listIterator.next(), list.get(index));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {10})
-    void listIteratorStartingOutOfRangeIndex(int index) {
+    void shouldThrowException_WhenListIteratorIndexedOutOfRange(int index) {
         Exception exception = assertThrows(IndexOutOfBoundsException.class, () -> {
-            ListIterator<Integer> listIterator = list.listIterator(index);
+            list.listIterator(index);
         });
         assertInstanceOf(IndexOutOfBoundsException.class, exception);
     }
 
     @Test
-    void subListExitingIndexes() {
+    void shouldReturnSubListOfElements_StartingSecond_EndingThird() {
         int fromIndex = 1;
         int toIndex = 3;
         List<Integer> subList = list.subList(fromIndex, toIndex);
         assertEquals(2, subList.size());
-        assertArrayEquals(subList.toArray(), Arrays.copyOfRange(items, fromIndex, toIndex));
+        assertArrayEquals(subList.toArray(), Arrays.copyOfRange(initialItemsList, fromIndex, toIndex));
     }
 
     @Test
-    void subListStartingIndexLessThenZero() {
+    void shouldThrowException_WhenSubListStartIndexLessThanZero() {
         Exception exception = assertThrows(IndexOutOfBoundsException.class, () -> {
             List<Integer> sublist = list.subList(-1, 3);
         });
@@ -170,7 +164,7 @@ class MyLinkedListTest {
     }
 
     @Test
-    void subListStartingIndexGreaterThenEnding() {
+    void shouldThrowException_WhenSubListStartIndexGreaterThanEndIndex() {
         Exception exception = assertThrows(IndexOutOfBoundsException.class, () -> {
             List<Integer> sublist = list.subList(2, 1);
         });
@@ -178,7 +172,7 @@ class MyLinkedListTest {
     }
 
     @Test
-    void subListEndingIndexGreaterThenListSize() {
+    void shouldThrowException_WhenSubListEndIndexGreaterThanListSize() {
         Exception exception = assertThrows(IndexOutOfBoundsException.class, () -> {
             List<Integer> sublist = list.subList(1, 10);
         });
@@ -186,125 +180,117 @@ class MyLinkedListTest {
     }
 
     @Test
-    void toArraySameAsInitialOne() {
+    void shouldReturnArrayOfElementsWithSameSequence() {
         Object[] resultArray = list.toArray();
-        assertArrayEquals(items, resultArray);
-    }
-    @Test
-    void toArrayProperSequenceAsInitialOne() {
-        Object[] resultArray = list.toArray();
-        assertArrayEquals(resultArray, items);
+        assertArrayEquals(initialItemsList, resultArray);
     }
 
     @Test
-    void toArrayNewArrayAllocated() {
-        Object[] resultArray = list.toArray();
-        assertNotSame(resultArray, items);
-    }
-
-    @Test
-    void addNull() {
-        boolean result = list.add(null);
-        assertTrue(result);
-        assertEquals(items.length + 1, list.size());
-        assertNull(list.getLast());
-    }
-
-    @Test
-    void addElement() {
+    void shouldAddNewElementToTheEnd() {
         Integer newElement = 40;
         boolean result = list.add(newElement);
         assertTrue(result);
-        assertEquals(items.length + 1, list.size());
+        assertEquals(initialItemsList.length + 1, list.size());
         assertEquals(newElement, list.getLast());
     }
 
     @ParameterizedTest
     @ValueSource(ints = {0})
-    void addElementAtPlaceByIndex(Integer index) {
+    void shouldAddNewElementAtPlaceByIndex_IndexedToFirstPosition(Integer index) {
         Integer newElement = 40;
-        list.add(index,newElement);
+        list.add(index, newElement);
+        assertEquals(initialItemsList.length + 1, list.size());
         assertEquals(newElement, list.get(index));
-        assertArrayEquals(list.subList(index + 1, list.size()).toArray(), items);
     }
 
     @Test
-    void addElementOutOfRangeIndex() {
+    void shouldThrowException_WhenNewElementIndexedOutOfRange() {
         Exception exception = assertThrows(IndexOutOfBoundsException.class, () -> {
             Integer newElement = 40;
+            int outOfRangeIndex = 999;
 
-            list.add(items.length + 1, newElement);
+            list.add(outOfRangeIndex, newElement);
         });
         assertInstanceOf(IndexOutOfBoundsException.class, exception);
     }
 
     @Test
-    void addCollectionToTheEnd() {
+    void shouldAddCollectionToTheEnd() {
         ArrayList<Integer> collection = new ArrayList<Integer>();
         collection.add(15);
         collection.add(25);
         collection.add(35);
         boolean result = list.addAll(collection);
         assertTrue(result);
-        assertEquals(items.length + collection.size(), list.size());
-        assertArrayEquals(list.subList(items.length, items.length + collection.size()).toArray(), collection.toArray());
+        assertEquals(initialItemsList.length + collection.size(), list.size());
+        assertArrayEquals(list.subList(initialItemsList.length, initialItemsList.length + collection.size()).toArray(), collection.toArray());
     }
 
     @Test
-    void addEmptyCollection() {
+    void shouldReturnFalse_WhenAddingEmptyCollection() {
         ArrayList<Integer> collection = new ArrayList<Integer>();
         boolean result = list.addAll(collection);
         assertFalse(result);
-        assertEquals(items.length, list.size());
+        assertEquals(initialItemsList.length, list.size());
     }
 
     @Test
-    void addCollectionAtPositionWithZeroIndex() {
+    void shouldAddCollectionAtSpecificPosition_IndexedToFirstElement() {
         ArrayList<Integer> collection = new ArrayList<Integer>();
         collection.add(15);
         collection.add(25);
         collection.add(35);
         boolean result = list.addAll(0, collection);
         assertTrue(result);
-        assertEquals(items.length + collection.size(), list.size());
+        assertEquals(initialItemsList.length + collection.size(), list.size());
         assertArrayEquals(list.subList(0, collection.size()).toArray(), collection.toArray());
     }
+
     @Test
-    void addCollectionAtPositionWithOutOfRangeIndex() {
+    void shouldThrowException_WhenAddingCollectionAtPositionIndexedOutOfRange() {
         Exception exception = assertThrows(IndexOutOfBoundsException.class, () -> {
             ArrayList<Integer> collection = new ArrayList<Integer>();
             collection.add(15);
             collection.add(25);
             collection.add(35);
+            int outOfRangeIndex = 999;
 
-            list.addAll(items.length + 1, collection);
+            list.addAll(outOfRangeIndex, collection);
         });
         assertInstanceOf(IndexOutOfBoundsException.class, exception);
     }
 
     @Test
-    void removeExistingElement() {
-        assertTrue(list.remove(items[0]));
-        assertEquals(items.length - 1, list.size());
+    void shouldRemoveExistingElement() {
+        assertTrue(list.remove(initialItemsList[0]));
+        assertEquals(initialItemsList.length - 1, list.size());
     }
 
     @Test
-    void removeNotExistingElement() {
-        Integer fakeElement = 5;
+    void shouldReturnFalse_WhenElementDoesNotExist() {
+        Integer fakeElement = 5555;
         assertFalse(list.remove(fakeElement));
-        assertEquals(items.length, list.size());
+        assertEquals(initialItemsList.length, list.size());
     }
 
     @ParameterizedTest
     @ValueSource(ints = {0})
-    void removeElementByIndex(int index) {
-        Integer oldValue = list.remove(index);
-        assertEquals(items.length - 1, list.size());
-        assertEquals(oldValue, items[index]);
+    void shouldRemoveElementByIndex(int index) {
+        list.remove(index);
+        assertEquals(initialItemsList.length - 1, list.size());
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0})
+    void shouldReturnOldValue_WhenElementRemovedByIndex(int index) {
+        Integer oldValue = list.remove(index);
+        assertEquals(oldValue, initialItemsList[index]);
+    }
+
+
     @ParameterizedTest
     @ValueSource(ints = {10})
-    void removeElementByOutOfRangeIndex(int index) {
+    void shouldThrowException_WhenRemoveByIndexOutOfRange(int index) {
         Exception exception = assertThrows(IndexOutOfBoundsException.class, () -> {
             list.remove(index);
         });
@@ -312,59 +298,52 @@ class MyLinkedListTest {
     }
 
     @Test
-    void removeCollection() {
-        assertTrue(list.removeAll(Arrays.asList(items)));
+    void shouldRemoveElementsSpecifiedInCollection() {
+        assertTrue(list.removeAll(Arrays.asList(initialItemsList)));
         assertEquals(0, list.size());
     }
 
     @Test
-    void removeEmptyCollection() {
+    void shouldReturnFalse_WhenPassedEmptyCollection() {
         ArrayList<Integer> emptyCollection = new ArrayList<>();
         assertFalse(list.removeAll(emptyCollection));
-        assertEquals(items.length, list.size());
+        assertEquals(initialItemsList.length, list.size());
     }
 
     @Test
-    void removeCollectionWithoutMatchElements() {
-        List<Integer> collection = List.of(1, 2, 3);
+    void shouldReturnFalse_WhenRemoveCollectionWithoutMatchElements() {
+        List<Integer> collection = List.of(100, 45, 18);
 
         assertFalse(list.removeAll(collection));
-        assertEquals(items.length, list.size());
+        assertEquals(initialItemsList.length, list.size());
     }
 
     @Test
-    void removeCollectionWithOneMatchElement() {
-        List<Integer> collection = List.of(1, 20, 3);
+    void shouldReturnTrue_WhenRemoveCollectionWithAtLeastOneMatchElement() {
+        List<Integer> collection = List.of(10, 20, 3);
+        int countElementsToBeDeleted = (int) list.stream().filter(collection::contains).count();
 
         assertTrue(list.removeAll(collection));
-        assertEquals(items.length - 1, list.size());
+        assertEquals(initialItemsList.length - countElementsToBeDeleted, list.size());
     }
 
     @Test
-    void retainElementsFromCollection() {
+    void shouldRetainOnlyElementsOfSpecifiedCollection() {
         List<Integer> collection = List.of(30, 20);
 
         assertTrue(list.retainAll(collection));
         assertEquals(2, list.size());
     }
 
-    @Test
-    void retainElementsFromTheSameCollection() {
-        List<Integer> collection = List.of(items);
-
-        assertFalse(list.retainAll(collection));
-        assertEquals(items.length, list.size());
-    }
-
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 2})
-    void getElementByIndex(int index) {
-        assertEquals(items[index], list.get(index));
+    void shouldReturnElementByIndex(int index) {
+        assertEquals(initialItemsList[index], list.get(index));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {10})
-    void getElementByIndexOutOfRange(int index) {
+    void shouldThrowException_WhenGettingElementByIndexOutOfRange(int index) {
         Exception exception = assertThrows(IndexOutOfBoundsException.class, () -> {
             list.get(index);
         });
@@ -373,16 +352,23 @@ class MyLinkedListTest {
 
     @ParameterizedTest
     @ValueSource(ints = {1})
-    void setElementByIndex(int index) {
+    void shouldSetElementByIndex(int index) {
         Integer newValue = 15;
-        Integer oldValue = list.set(index, newValue);
-        assertEquals(oldValue, items[index]);
+        list.set(index, newValue);
         assertEquals(newValue, list.get(index));
     }
 
     @ParameterizedTest
+    @ValueSource(ints = {1})
+    void shouldReturnOldValue_WhenSetElementByIndex(int index) {
+        Integer newValue = 15;
+        Integer oldValue = list.set(index, newValue);
+        assertEquals(oldValue, initialItemsList[index]);
+    }
+
+    @ParameterizedTest
     @ValueSource(ints = {10})
-    void setElementByIndexOutOfRange(int index) {
+    void shouldThrowException_WhenSettingElementByIndexOutOfRange(int index) {
         Exception exception = assertThrows(IndexOutOfBoundsException.class, () -> {
             list.set(index, 15);
         });
@@ -390,31 +376,31 @@ class MyLinkedListTest {
     }
 
     @Test
-    void pushToHead() {
+    void shouldPushNewElementToHead() {
         Integer newElement = 12;
         assertTrue(list.pushToHead(newElement));
-        assertEquals(items.length + 1, list.size());
+        assertEquals(initialItemsList.length + 1, list.size());
         assertEquals(newElement, list.get(0));
     }
 
     @Test
-    void getFirstElement() {
-        assertEquals(items[0], list.getFirst());
+    void shouldReturnFirstElement() {
+        assertEquals(initialItemsList[0], list.getFirst());
     }
 
     @Test
-    void getFirstElementNull() {
+    void shouldReturnNull_WhenGettingFirstElementOnEmptyList() {
         list.clear();
         assertNull(list.getFirst());
     }
 
     @Test
-    void getLastElement() {
-        assertEquals(items[items.length - 1], list.getLast());
+    void shouldReturnLastElement() {
+        assertEquals(initialItemsList[initialItemsList.length - 1], list.getLast());
     }
 
     @Test
-    void getLastElementNull() {
+    void shouldReturnNull_WhenGettingLastElementOnEmptyList() {
         list.clear();
         assertNull(list.getLast());
     }
