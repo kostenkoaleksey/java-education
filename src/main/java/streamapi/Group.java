57e636ee18803fb3318da68cd419baba886efbe2
Oleksii Kostenko <aleksey.kostenko@p2h.com>
2023-07-25 16:17:09 +0300

    Setup lombok

--- src/main/java/streamapi/Group.java
+++ src/main/java/streamapi/Group.java
@@ -1,27 +1,13 @@
 package streamapi;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class Group {
     private int id;
     private String title;
-
-    public Group(int id, String title) {
-        this.id = id;
-        this.title = title;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 }
--- src/main/java/streamapi/Student.java
+++ src/main/java/streamapi/Student.java
@@ -1,57 +1,16 @@
 package streamapi;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class Student {
     private int id;
     private String firstName;
     private String lastName;
     private short age;
     private String sex;
-
-    public Student(int id, String firstName, String lastName, short age, String sex) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.sex = sex;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public short getAge() {
-        return age;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setAge(short age) {
-        this.age = age;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
 }
